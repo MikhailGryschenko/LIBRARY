540484bf20ad7d1fd3d886f1abf901491081b1a6
Mikhail <maybeminsk@gmail.com>
2023-12-25 22:10:41 +0300

    fadeToggle

--- src/js/lib/modules/effects.js
+++ src/js/lib/modules/effects.js
@@ -29,7 +29,6 @@ $.prototype.fadeIn = function(dur, display, fin) {
     for (let i = 0; i < this.length; i++) {
         this[i].style.display = display || 'block';
 
-
         const _fadeIn = (complection) => {
             this[i].style.opacity = complection;
         };
@@ -44,7 +43,6 @@ $.prototype.fadeOut = function(dur, display, fin) {
     for (let i = 0; i < this.length; i++) {
         this[i].style.display = display || 'block';
 
-
         const _fadeOut = (complection) => {
             this[i].style.opacity = 1 - complection;
             if(complection === 1) {
@@ -57,3 +55,33 @@ $.prototype.fadeOut = function(dur, display, fin) {
     }
     return this;
 };
+
+$.prototype.fadeToggle = function(dur, display, fin) {
+    for (let i = 0; i < this.length; i++) {
+        if (window.getComputedStyle(this[i]).display === 'none') {
+            this[i].style.display = display || 'block';
+
+            const _fadeIn = (complection) => {
+                this[i].style.opacity = complection;
+            };
+    
+            const ani = this.animateOverTime(dur, _fadeIn, fin);
+            requestAnimationFrame(ani);
+        } else {
+            this[i].style.display = display || 'block';
+
+            const _fadeOut = (complection) => {
+                this[i].style.opacity = 1 - complection;
+                if(complection === 1) {
+                    this[i].style.display = 'none';
+                }
+            };
+    
+            const ani = this.animateOverTime(dur, _fadeOut, fin);
+            requestAnimationFrame(ani);
+        }
+
+    }
+    return this;
+};
+
