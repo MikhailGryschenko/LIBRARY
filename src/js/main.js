d0c42c6f6ffccec5aa7afd4365d1615ec213e07c
Mikhail <maybeminsk@gmail.com>
2024-01-08 21:20:45 +0300

    modal dinamically

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,28 @@ $('[data-count="second"]').on('click', () => {
 $('button').eq(2).on('click', () => {
     $('.w-500').fadeToggle(800);
 });
+
+$('#trigger').click(() => $('#trigger').createModal({
+    text: {
+        title: 'Modal title',
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium cumque porro vero aliquid autem fuga dicta dolor atque exercitationem consectetur nulla modi obcaecati blanditiis architecto itaque sunt distinctio, quae ea.'
+    },
+    btns: {
+        count: 2,
+        settings: [
+            [
+                'Close',
+                ['btn-danger', 'mr-10'],
+                true
+            ],
+            [
+                'Btn changes',
+                ['btn-success'],
+                false,
+                () => {
+                    alert('Данные сохранены');
+                }
+            ]
+        ]
+    }
+}));
